da7ecb1b8f80fe1771431bd622f9e7e9f2dc1c88
IvanTobilko <ivan.tobilko@gmail.com>
2022-02-06 22:50:26 -0800

    lessin 1 JS

---
+++ main.js
@@ -0,0 +1,113 @@
+let numbers = [1, 10, -999, 123, 3.14, 2.7, 16, true, false, 'hello', 'owu', 'ua'];
+
+console.log (numbers[0]);
+console.log (numbers[1]);
+console.log (numbers[2]);
+console.log (numbers [3]);
+console.log (numbers [4]);
+console.log (numbers[5]);
+console.log (numbers[6]);
+console.log (numbers[7]);
+console.log (numbers[8]);
+console.log (numbers[9]);
+console.log (numbers[10]);
+console.log (numbers[11]);
+console.log (numbers[12]);
+//
+// alert (numbers[0]);
+// alert (numbers[1]);
+// alert (numbers[2]);
+// alert (numbers [3]);
+// alert (numbers [4]);
+// alert(numbers [5]);
+// alert(numbers [6]);
+// alert(numbers [7]);
+// alert(numbers [8]);
+// alert(numbers [9]);
+// alert(numbers [10]);
+// alert(numbers [11]);
+// alert(numbers [12]);
+
+
+// let PI = 3.14;
+// PI = 1231232;
+// console.log(PI);
+
+let A = 'owu';
+A = 'school';
+console.log(A);
+
+let changers2 = 'ua';
+changers2 = 'country';
+console.log(changers2);
+
+let changers3 = 1;
+changers3 = -1;
+console.log(changers3);
+
+let changers4 = 10;
+changers4 = 20;
+console.log(changers4);
+
+let changers5 = -999;
+changers5 = 999;
+console.log(changers5);
+
+let changers6 = 123;
+changers6 = 321;
+console.log(changers6);
+
+let changers7 = 3.14;
+changers7 = 'PI';
+console.log(changers7);
+
+let changers8 = 2.7;
+changers8 = 7.2;
+console.log(changers8);
+
+let changers9 = 16;
+changers9 = 61;
+console.log(changers9);
+
+let changers10 = 'true';
+changers10 = '-true';
+console.log(changers10);
+
+let changers11 = 'false';
+changers11 = '-false';
+console.log(changers11);
+
+// alert (A);
+// alert (changers2);
+// alert (changers3);
+// alert (changers4);
+// alert (changers5);
+// alert(changers6);
+// alert(changers7);
+// alert(changers8);
+// alert(changers9);
+// alert(changers10);
+// alert(changers11);
+
+
+let a = 100;
+let b = '100';
+let c = true;
+console.log (typeof a);
+console.log (typeof b);
+console.log (typeof c);
+
+console.log(5<6);
+console.log(5>6);
+console.log(5===6);
+console.log (5===6);
+console.log (10<=10);
+console.log (10===10);
+console.log (10!==10);
+console.log (10<10);
+console.log (10>10);
+console.log (123==='123');
+console.log (123<='123');
+
+
+
